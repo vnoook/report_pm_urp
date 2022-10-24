dec5a3d92afa31c751343536de6c59ab5f1a41b7
vnoook <noook@yandex.ru>
2022-10-24 20:55:10 +0700

    #8 подготовка к записи данных в файл

--- report_pm_urp.py
+++ report_pm_urp.py
@@ -42,17 +42,20 @@ wb_stat = openpyxl.load_workbook(file_stat)
 # заполняемый файл
 wb_pm_urp = openpyxl.load_workbook(file_pm_urp)
 
+# получение данных из файла источника
+# список для хранения данных из источника
 data_from = []
-
 # цикл прохода по разделам (листам) в заполняемом файле
 for wb_pm_urp_sheet, wb_pm_urp_data in dict_data.items():
-    print(wb_pm_urp_sheet)
+    # print(wb_pm_urp_sheet)
+
+    # выбор активного листа в файле
     wb_pm_urp_s = wb_pm_urp[wb_pm_urp_sheet]
 
     # цикл прохода по листам файла источника данных
     for wb_stat_sheet, wb_stat_data in wb_pm_urp_data.items():
-        print('       ' + wb_stat_sheet)
-        # беру лист в фале с данными
+        # print('       ' + wb_stat_sheet)
+        # беру лист в файле с данными
         wb_stat_s = wb_stat[wb_stat_sheet]
 
         # диапазон ячеек из файла статистики
@@ -66,23 +69,31 @@ for wb_pm_urp_sheet, wb_pm_urp_data in dict_data.items():
         wb_cells_range_from = wb_stat_s[cells_range_from]
         for row_in_range in wb_cells_range_from:
             for cell_in_row in row_in_range:
-                # print(cell_in_row)
                 indexR = wb_cells_range_from.index(row_in_range)
                 indexC = row_in_range.index(cell_in_row)
                 temp_list.append(wb_cells_range_from[indexR][indexC].value)
             data_from.append(temp_list)
             temp_list = []
 
-        # записываю информацию
-        # назначаю диапазон ячеек
-        wb_cells_range_to = wb_pm_urp_s[cells_range_to]
-        # print(wb_cells_range_to)
-        for row_in_range in wb_cells_range_to:
-            for cell_in_row in row_in_range:
-                print(cell_in_row, end=' = ')
-                indexR = wb_cells_range_to.index(row_in_range)
-                indexC = row_in_range.index(cell_in_row)
-                print(wb_cells_range_to[indexR][indexC].value)
+
+
+
+
+
+
+
+
+
+# # записываю информацию
+# # назначаю диапазон ячеек
+# wb_cells_range_to = wb_pm_urp_s[cells_range_to]
+# # print(wb_cells_range_to)
+# for row_in_range in wb_cells_range_to:
+#     for cell_in_row in row_in_range:
+#         print('            ', cell_in_row, end=' = ')
+#         indexR = wb_cells_range_to.index(row_in_range)
+#         indexC = row_in_range.index(cell_in_row)
+#         print(wb_cells_range_to[indexR][indexC].value)
 
 
 
